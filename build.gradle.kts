9e9d7c97529cb2a305e311ca6cbf20a785c7afc1
Zach Tibbitts <zachtib@gmail.com>
2022-07-12 09:29:24 -0400

    fix some formatting in gradle scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,16 @@
 group = "com.zachtib"
 version = "1.0-SNAPSHOT"
 
+// KTIJ-19369
+// https://youtrack.jetbrains.com/issue/KTIJ-19369
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.compose) apply false
+    alias(libs.plugins.kotlinx.serialization) apply false
+    alias(libs.plugins.ktlint)
+}
+
 tasks.create<Delete>("clean") {
     delete(rootProject.buildDir)
 }
